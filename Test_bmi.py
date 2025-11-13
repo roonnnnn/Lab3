3f51071e54b724eacbcf046b9e1923e1c17232e0
Ron Lee <RONLEE.23@ichat.sp.edu.sg>
2025-11-13 11:03:05 +0800

    Created Test_bmi.py file

---
+++ Test_bmi.py
@@ -0,0 +1,18 @@
+import lab2_2520.bmi as ABC
+
+def test_calcBMI_underweight():
+    expected = -1
+    result = ABC.calculate_bmi(1.73,37)
+    assert(result == expected)
+
+def test_calcBMI_normalweight():
+    expected = 0
+    result = ABC.calculate_bmi(1.73,57)
+    assert(result == expected)
+
+def test_calcBMI_overweight():
+    expected = 1
+    result = ABC.calculate_bmi(1.73,97)
+    assert(result == expected)
+
+
